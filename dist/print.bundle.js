493f1c0981ed3546d64c63048cd117c237f3858c
JavanFriedel <javanfriedel@hotmail.com>
2022-03-12 17:15:29 -0500

    Added index.html reactive compilation for script tags

--- dist/print.bundle.js
+++ dist/print.bundle.js
@@ -10,51 +10,20 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/index.js":
+/***/ "./src/print.js":
 /*!**********************!*\
-  !*** ./src/index.js ***!
+  !*** ./src/print.js ***!
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _myName__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./myName */ \"./src/myName.js\");\n\n\nfunction component() {\n  const element = document.createElement(\"div\");\n\n  element.textContent = (0,_myName__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(\"Cody\");\n\n  return element;\n}\n\ndocument.body.appendChild(component());\n\n\n//# sourceURL=webpack://webpack-demo/./src/index.js?");
-
-/***/ }),
-
-/***/ "./src/myName.js":
-/*!***********************!*\
-  !*** ./src/myName.js ***!
-  \***********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst myName = (name) => \"Hi! My name is \" + name;\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (myName);\n\n\n//# sourceURL=webpack://webpack-demo/./src/myName.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ printMe)\n/* harmony export */ });\nfunction printMe() {\n  console.log(\"I get called from print.js!\");\n}\n\n\n//# sourceURL=webpack://webpack-demo/./src/print.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/define property getters */
@@ -90,7 +59,8 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/print.js"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
